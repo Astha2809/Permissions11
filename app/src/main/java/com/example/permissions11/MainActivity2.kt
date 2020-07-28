c086589b6d65b227661f6bd9da5467fa104c1f2a
astha@astha <astha.singh@tothenew>
2020-07-28 16:11:08 +0530

    rationale and alertbox added

--- app/src/main/java/com/example/permissions11/MainActivity2.kt
+++ app/src/main/java/com/example/permissions11/MainActivity2.kt
@@ -3,14 +3,14 @@ package com.example.permissions11
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
+import android.provider.Settings
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -31,14 +31,17 @@ class MainActivity2 : AppCompatActivity() {
     fun requestLocationPermission() {
         if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)) {
             Toast.makeText(this, "location permission required", Toast.LENGTH_LONG).show()
-            requestPermissions(
-                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                100
-            )
+//            requestPermissions(
+//                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+//                100
+//            )
+            moveToSettingsDialouge()
+            //add dialouge that would move to settings
 
         } else {
             Toast.makeText(this, "location permission not available", Toast.LENGTH_LONG).show()
             requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+            //moveToSettingsDialouge()
 
         }
     }
@@ -54,11 +57,36 @@ class MainActivity2 : AppCompatActivity() {
                 Toast.makeText(this, "location permission granted", Toast.LENGTH_LONG).show()
 
             } else {
-                Toast.makeText(this, "location permission important", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "location permission important for following features", Toast.LENGTH_LONG).show()
+                //moveToSettingsDialouge()
 
             }
         }
 
+    }
+
+    fun moveToSettingsDialouge(){
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Permission Mandatory for the following feature")
+            .setMessage("Change settings")
+            .setPositiveButton("OK"){dialog,which->
+                onPositiveClicked()
+
+            }
+            .setNegativeButton("Cancel"){dialog,which->
+
+            }
+            .show()
+
+    }
+
+    fun onPositiveClicked(){
+        val settingIntent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", packageName, null)
+        )
+        settingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(settingIntent)
     }
 
 
